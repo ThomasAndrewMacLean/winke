2821f6a9ffbc07f45e6fce5d497b1c926aa8e3eb
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-05-24 22:26:16 +0200

    add help cursor nudge

--- components/Image.js
+++ components/Image.js
@@ -1,16 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { PictureContext } from '../utils/contexts';
 import PropTypes from 'prop-types';
 import { getImageUrl, getImageName } from '../utils';
 import styled from 'styled-components';
 
 const Image = ({ imageId, showName }) => {
+  const [click, clickSet] = useState(false);
   const pics = useContext(PictureContext);
   return (
-    <ImageWrap>
+    <ImageWrap showName={showName} onClick={() => clickSet(!click)}>
       <img src={getImageUrl(pics, imageId, true)}></img>
 
-      {showName && (
+      {showName && click && (
         <ImageName>
           <span>{getImageName(pics, imageId)}</span>
         </ImageName>
@@ -21,6 +22,7 @@ const Image = ({ imageId, showName }) => {
 
 const ImageWrap = styled.div`
   position: relative;
+  cursor: ${(props) => props.showName && 'help'};
 `;
 const ImageName = styled.span`
   background: #ffffff80;
