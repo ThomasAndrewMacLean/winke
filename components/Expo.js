d24e94a9cadacca6e4677476626ee9ee83d56c1f
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-05-24 22:16:41 +0200

    add youtube

--- components/Expo.js
+++ components/Expo.js
@@ -10,6 +10,16 @@ const ExpoSection = () => {
       </h2>
 
       <T translationKey="expoText" />
+
+      <iframe
+        style={{ marginTop: '3rem' }}
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/4m4Aq7_DXrY?controls=0"
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </Section>
   );
 };
