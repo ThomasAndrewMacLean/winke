634ad371e5071254502d6dd87be029d8f5457558
ThomasAndrewMacLean <thomas.maclean@gmail.com>
2020-06-30 19:20:54 +0000

    Deploying to gh-pages from  @ 60b68d6c67f91e1ee67daac357f25e8db67c7728 🚀

---
+++ _next/static/Hz91iKZ-w01UPUgOh48Dj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/privacy":[a]}}("static\u002Fchunks\u002F0760efa230ccca8b1c8d2774ed52f58f8114babb.809805d075130808b25e.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
